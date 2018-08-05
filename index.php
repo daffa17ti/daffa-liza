ab07e7ee7407ed9e7e8aef09b16bcf0ed2b45871
Elliza Megawati Ritonga <38091851+elliza123@users.noreply.github.com>
2018-08-05 09:23:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php session_start();?>
 <html>
 	<head>
-		<title>Elimination GAUSS JORDAN</title>
+		<title>Elimination GAUSS </title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<link rel="stylesheet" href="assets/css/main.css" />
